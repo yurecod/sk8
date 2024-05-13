1c848801c38b72e0991133bf0d20b39fb68d3d10
Yure <yurecod@gmail.com>
2024-05-13 14:06:51 -0400

    fixed menu lateral 2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const botaoMenu = document.querySelector('.cabecalho__menu')
 const menu = document.querySelector('.menu-lateral')
+// const hiddenMenu = document.querySelector()
 
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
 })
+
